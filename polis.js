9d8b43636d7e9da31632a1eb6e54e5d0b234255e
Kyle Tolle <kyle@nullsix.com>
2013-12-09 11:45:42 -0700

    Create handlers for GET and POST verbs
    GET and POST requests are processed by their own handlers. This reduces
    duplication and makes things more extensible.

--- polis.js
+++ polis.js
@@ -6,43 +6,53 @@ var SECONDS_TO_SLEEP = 0;
 
 app.listen(LISTENING_PORT);
 
+handlers = {"POST": PostHandler, "GET": GetHandler};
+
 function handler (req, res) {
-  if (req.method == "POST") {
-    res.writeHead(STATUS_CODE, {'Content-Type': 'application/json'});
+  var handler = new handlers[req.method];
 
-    var data = '';
-    req.on('data', function(chunk) {
-      data += chunk;
-    });
-
-    req.on('end', function() {
-      console.log('Logging POST request:');
-
-      console.log('Headers:');
-      console.log(req.headers);
-
-      console.log('Body:');
-      console.log(data.toString());
-      console.log();
-    });
-  }
-
-  if (req.method == "GET") {
-    res.writeHead(STATUS_CODE, {'Content-Type': 'text/html'});
-
-    res.write("<html><head><title>Polis.js</title></head><body><h2>Polis.js</h2><p>Running!</p></body></html>");
-
-    req.on('end', function() {
-      console.log('Logging GET request:');
-
-      console.log('Headers:');
-      console.log(req.headers);
-      console.log();
-    });
-  }
+  handler.handle(req, res);
 
   sleep.sleep(SECONDS_TO_SLEEP);
   res.end();
+}
+
+function PostHandler() { }
+
+PostHandler.prototype.handle = function(req, res) {
+  res.writeHead(STATUS_CODE, {'Content-Type': 'application/json'});
+
+  var data = '';
+  req.on('data', function(chunk) {
+    data += chunk;
+  });
+
+  req.on('end', function() {
+    console.log('Logging POST request:');
+
+    console.log('Headers:');
+    console.log(req.headers);
+
+    console.log('Body:');
+    console.log(data.toString());
+    console.log();
+  });
+}
+
+function GetHandler() { }
+
+GetHandler.prototype.handle = function(req, res) {
+  res.writeHead(STATUS_CODE, {'Content-Type': 'text/html'});
+
+  res.write("<html><head><title>Polis.js</title></head><body><h2>Polis.js</h2><p>Running!</p></body></html>");
+
+  req.on('end', function() {
+    console.log('Logging GET request:');
+
+    console.log('Headers:');
+    console.log(req.headers);
+    console.log();
+  });
 }
 
 console.log("Listening to port " + LISTENING_PORT);
